4c3b2df53b85f3cecc680937e2f82f97e589ae48
Baicheng CHEN <baichengchen@Dannys-MacBook-Pro.local>
2026-03-04 08:31:12 +0800

    update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,4 @@
  * Update CV_URL here to change the CV link across all pages.
  */
 export const CV_URL =
-  "https://drive.google.com/file/d/1DNXsDYs0uFKCrUesdeW_q5eCmFAxEy3n/view?usp=sharing";
+  "#";
